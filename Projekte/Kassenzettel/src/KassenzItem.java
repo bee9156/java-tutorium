6768f55382af3872d0fa7d18036469b5634409f0
Benjamin Ewert <lars@vmmint13-ss14.(none)>
2014-11-04 16:19:16 +0100

    added items to sort

---
+++ Projekte/Kassenzettel/src/KassenzItem.java
@@ -0,0 +1,28 @@
+
+public class KassenzItem implements Comparable<KassenzItem> {
+
+	public int anzahl;
+	public double preis;
+	public String name;
+	
+	KassenzItem(int a, double p, String n){
+		anzahl = a;
+		preis = p;
+		name = n;
+	}
+	
+	public int compareTo(KassenzItem k){
+		int result = k.anzahl - anzahl;
+		
+		if 
+		
+		return result;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
